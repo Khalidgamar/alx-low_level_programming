8d5bdbb18bc8cca99bf434d296f51293c6b98659
Khalidgamar <khgamar97@gmail.com>
2022-10-20 11:25:27 -0700

    task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Test function for positive or negative
  *
@@ -6,8 +9,15 @@
  */
 int main(void)
 {
-int i;
+positive_or_negative(int i)
+int n;
 i = 0;
-positive_or_negative(i);
+n = i % 2;
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else
+printf("%d is negative\n", n);
 return (0);
 }
